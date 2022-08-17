03e61688d9920be4bb1bc288494bd4e2cb0e4f47
januszpolak <janusz.polak@interia.pl>
2022-08-17 11:17:12 +0200

    add positions variables from url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,17 +12,29 @@ export class AppComponent {
   // ISS current location API url
   url = 'http://api.open-notify.org/iss-now.json';
 
+  // positions variables
+  latitude: any = '';
+  longitude: any = '';
 
   constructor(private http:HttpClient){}
 
   ngOnInit(){
 
     this.getLocation();
+    
   }
 
   getLocation(){
-    this.http.get(this.url).subscribe((res:any)=>
-    console.log(res))
+    this.http.get(this.url).subscribe((res:any)=>{
+      console.log(res.iss_position.latitude, res.iss_position.longitude);
+
+      // positions variables from url
+      this.latitude = res.iss_position.latitude;
+      this.longitude = res.iss_position.longitude;
+    })
+    
+    
+
   }
 
 
