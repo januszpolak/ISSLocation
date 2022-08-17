c6ceeee780bb80f5cb7a5eb9341eb3065d083e42
januszpolak <janusz.polak@interia.pl>
2022-08-17 10:58:46 +0200

    add getLocation function to get location in console.log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ISSLocation';
+
+  // ISS current location API url
+  url = 'http://api.open-notify.org/iss-now.json';
+
+
+  constructor(private http:HttpClient){}
+
+  ngOnInit(){
+
+    this.getLocation();
+  }
+
+  getLocation(){
+    this.http.get(this.url).subscribe((res:any)=>
+    console.log(res))
+  }
+
+
+
+
+
+
+
+
+
 }
