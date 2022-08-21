462432964be185215cfb1fdb274dc20c513538eb
januszpolak <janusz.polak@interia.pl>
2022-08-21 17:14:13 +0200

    generate astro component to show how many astronauts are in space

---
+++ src/app/astro/astro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-astro',
+  templateUrl: './astro.component.html',
+  styleUrls: ['./astro.component.css']
+})
+export class AstroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
